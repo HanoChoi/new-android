d3436afc332b95ffc1a62788c618adf3efe60f79
hano <777655256@naver.com>
2024-02-26 10:58:56 +0900

    hilt build

---
+++ app/src/main/java/com/hano/general3/App.kt
@@ -0,0 +1,8 @@
+package com.hano.general3
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+}
--- features/splash/splash-di/src/main/java/com/hano/splash_di/ModelAModule.kt
+++ features/splash/splash-di/src/main/java/com/hano/splash_di/ModelAModule.kt
@@ -1,7 +1,3 @@
 package com.hano.splash_di
-
-import dagger.Module
-
-@Module
 interface ModelAModule {
 }
